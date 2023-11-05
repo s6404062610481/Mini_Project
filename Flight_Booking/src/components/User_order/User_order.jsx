5ef58cb6c83f82a0c4eac0a44cdc37e855fc6674
BigcBow <s6404062610481@email.kmutnb.ac.th>
2023-11-05 22:04:20 +0700

    add form user-order

--- Flight_Booking/src/components/User_order/User_order.jsx
+++ Flight_Booking/src/components/User_order/User_order.jsx
@@ -87,6 +87,126 @@ function User_order() {
                         </div>
                     </div>
                 </div>
+                <div className="ticket">
+                    <div className="ticket-form-user-order">
+                        <div className="main-user-order">
+                            <div className="content-user-order">
+                                <div className="booking-user-order">
+                                    <div className="booking-data-user-order">
+                                        Booking ID
+                                    </div>
+                                    <span>1</span>
+                                </div>
+                                <div className="name-flight">
+                                    <div className="name-flight-data-user-order">
+                                        FLIGHT INFORMATION
+                                    </div>
+                                </div>
+                                <div className="flight-id">
+                                    <div className="flight-id-data">
+                                        Flight ID
+                                    </div>
+                                    <span>001233</span>
+                                </div>
+                            </div>
+
+                            <div className="img-user-order">
+                                <div className="img-data-user-order">
+                                    <img src={qrcode2} alt="" />
+                                </div>
+                            </div>
+                        </div>
+                        <hr className='hr-user-order' />
+                        <div className="destination">
+                            <div className="des-to">
+                                <div className="des-to-data">
+                                    To
+                                </div>
+                                <span>Puket</span>
+                            </div>
+                            <div className="des-seat">
+                                <div className="dses-seat-data">
+                                    Seat
+                                </div>
+                                <span>B08</span>
+                            </div>
+                        </div>
+                        <div className="date-time-user-order">
+                            <div className="date-user-order">
+                                <div className="date-data-user-order">
+                                    Date
+                                </div>
+                                <span>01/12/2566</span>
+                            </div>
+                            <div className="time-user-order">
+                                <div className="time-date-user-order">
+                                    Time
+                                </div>
+                                <span>09:30:00</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="ticket">
+                    <div className="ticket-form-user-order">
+                        <div className="main-user-order">
+                            <div className="content-user-order">
+                                <div className="booking-user-order">
+                                    <div className="booking-data-user-order">
+                                        Booking ID
+                                    </div>
+                                    <span>1</span>
+                                </div>
+                                <div className="name-flight">
+                                    <div className="name-flight-data-user-order">
+                                        FLIGHT INFORMATION
+                                    </div>
+                                </div>
+                                <div className="flight-id">
+                                    <div className="flight-id-data">
+                                        Flight ID
+                                    </div>
+                                    <span>001233</span>
+                                </div>
+                            </div>
+
+                            <div className="img-user-order">
+                                <div className="img-data-user-order">
+                                    <img src={qrcode2} alt="" />
+                                </div>
+                            </div>
+                        </div>
+                        <hr className='hr-user-order' />
+                        <div className="destination">
+                            <div className="des-to">
+                                <div className="des-to-data">
+                                    To
+                                </div>
+                                <span>Puket</span>
+                            </div>
+                            <div className="des-seat">
+                                <div className="dses-seat-data">
+                                    Seat
+                                </div>
+                                <span>B08</span>
+                            </div>
+                        </div>
+                        <div className="date-time-user-order">
+                            <div className="date-user-order">
+                                <div className="date-data-user-order">
+                                    Date
+                                </div>
+                                <span>01/12/2566</span>
+                            </div>
+                            <div className="time-user-order">
+                                <div className="time-date-user-order">
+                                    Time
+                                </div>
+                                <span>09:30:00</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
             </div>
         </div>
