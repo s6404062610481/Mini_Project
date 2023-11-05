b056a33bf5d79d87f8ec95f57ed577231a43ee34
BigcBow <s6404062610481@email.kmutnb.ac.th>
2023-11-05 21:01:34 +0700

    edit form user_order

--- Flight_Booking/src/components/User_order/User_order.jsx
+++ Flight_Booking/src/components/User_order/User_order.jsx
@@ -28,76 +28,53 @@ function User_order() {
 
                 <div className="ticket">
                     <div className="ticket-form-user-order">
-                        <div className="goto">
-                            ID : 1
+                        <div className="booking-user-order">
+                            <div className="booking-data-user-order">
+                                Booking ID
+                            </div>
+                            <span>1</span>
                         </div>
-                        <div className="date">
-                            Airline : Nokair
+                        <br />
+                        <div className="name-flight">
+                            <div className="name-flight-data-user-order">
+                                FLIGHT INFORMATION
+                            </div>
                         </div>
-                        <div className="date">
-                            Destination : Puket
+                        <br />
+                        <div className="flight-id">
+                            <div className="flight-id-data">
+                                Flight ID
+                            </div>
+                            <span>001233</span>
                         </div>
-                        <div className="date">
-                            Date : 01/11/2024
+                        <hr />
+                        <div className="destination">
+                            <div className="des-to">
+                                <div className="des-to-data">
+                                    To
+                                </div>
+                                <span>Puket</span>
+                            </div>
+                            <div className="des-seat">
+                                <div className="dses-seat-data">
+                                    Seat
+                                </div>
+                                <span>B08</span>
+                            </div>
                         </div>
-                        <div className="time">
-                            Time : 09:30:00
-                        </div>
-                        <div className="time">
-                            Seat : A1
-                        </div>
-                        <div className="time">
-                            Price : 1300 Bath
-                        </div>
-                    </div>
-                </div>
-                <div className="ticket">
-                    <div className="ticket-form-user-order">
-                        <div className="goto">
-                            ID : 1
-                        </div>
-                        <div className="date">
-                            Airline : Nokair
-                        </div>
-                        <div className="date">
-                            Destination : Puket
-                        </div>
-                        <div className="date">
-                            Date : 01/11/2024
-                        </div>
-                        <div className="time">
-                            Time : 09:30:00
-                        </div>
-                        <div className="time">
-                            Seat : A1
-                        </div>
-                        <div className="time">
-                            Price : 1300 Bath
-                        </div>
-                    </div>
-                </div>
-                <div className="ticket">
-                    <div className="ticket-form-user-order">
-                        <div className="goto">
-                            ID : 1
-                        </div>
-                        <div className="date">
-                            Airline : Nokair
-                        </div>
-                        <div className="date">
-                            Destination : Puket
-                        </div>
-                        <div className="date">
-                            Date : 01/11/2024
-                        </div>
-                        <div className="time">
-                            Time : 09:30:00
-                        </div>
-                        <div className="time">
-                            Seat : A1
-                        </div>
-                        <div className="time">
-                            Price : 1300 Bath
+                        <div className="date-time-user-order">
+                            <div className="date-user-order">
+                                <div className="date-data-user-order">
+                                    Date
+                                </div>
+                                <span>01/12/2566</span>
+                            </div>
+                            <div className="time-user-order">
+                                <div className="time-date-user-order">
+                                    Time
+                                </div>
+                                <span>09:30:00</span>
+                            </div>
                         </div>
                     </div>
                 </div>
