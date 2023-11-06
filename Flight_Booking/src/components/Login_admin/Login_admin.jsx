cf39a71a58187fb1c2f5a70fffe75dea11ba4f35
BigcBow <s6404062610481@email.kmutnb.ac.th>
2023-11-07 00:10:28 +0700

    add form login admin

--- Flight_Booking/src/components/Login_admin/Login_admin.jsx
+++ Flight_Booking/src/components/Login_admin/Login_admin.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 
 function Login_admin() {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <div>
+            <div className="main-login-admin">
+                <div className="content-login-admin">
+                    <h2>Login Admin</h2>
+                    <div className="username-login-admin">
+                        Username : <input type="text" />
+                    </div>
+                    <div className="password-login-admin">
+                        Password : <input type="text" />
+                    </div>
+                </div>
+            </div>
+        </div>  
+    )
 }
 
 export default Login_admin
